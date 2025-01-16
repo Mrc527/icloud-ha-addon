d590e0fe24c5dd90745fcc809bd20975a5a2f652
Marco Visin <marco@visin.ch>
2025-01-16 22:07:21 +0100

    Working?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 echo "Starting..."
 # Check if config.json exists and parse it
-if [ -f /data/config.yaml ]; then
-    echo "found config.yaml"
-    cat /data/config.yaml
-    export PUID=$(yq e '.options.PUID' /data/config.yaml)
-    export PGID=$(yq e '.options.PGID' /data/config.yaml)
-    export TZ=$(yq e '.options.TZ' /data/config.yaml)
-    export ENV_CONFIG_FILE_PATH=$(yq e '.options.ENV_CONFIG_FILE_PATH' /data/config.yaml)
+if [ -f /data/options.yaml ]; then
+    echo "found options.yaml"
+    cat /data/options.yaml
+    export PUID=$(yq e '.options.PUID' /data/options.yaml)
+    export PGID=$(yq e '.options.PGID' /data/options.yaml)
+    export TZ=$(yq e '.options.TZ' /data/options.yaml)
+    export ENV_CONFIG_FILE_PATH=$(yq e '.options.ENV_CONFIG_FILE_PATH' /data/options.yaml)
 # Otherwise, check if config.yaml exists and parse it
-elif [ -f /data/config.json ]; then
-        echo "found config.json"
-        cat /data/config.json
-        export PUID=$(jq -r '.options.PUID' /data/config.json)
-        export PGID=$(jq -r '.options.PGID' /data/config.json)
-        export TZ=$(jq -r '.options.TZ' /data/config.json)
-        export ENV_CONFIG_FILE_PATH=$(jq -r '.options.ENV_CONFIG_FILE_PATH' /data/config.json)
+elif [ -f /data/options.json ]; then
+        echo "found options.json"
+        cat /data/options.json
+        export PUID=$(jq -r '.options.PUID' /data/options.json)
+        export PGID=$(jq -r '.options.PGID' /data/options.json)
+        export TZ=$(jq -r '.options.TZ' /data/options.json)
+        export ENV_CONFIG_FILE_PATH=$(jq -r '.options.ENV_CONFIG_FILE_PATH' /data/options.json)
 else
     echo "No configuration file found. Please provide either config.json or config.yaml."
-    tail -f /config/config.yaml
     exit 1
 fi
 
